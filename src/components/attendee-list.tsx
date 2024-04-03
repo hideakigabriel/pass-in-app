9098001f150cff91064caab46bda3bf82578128f
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-04-03 19:56:24 -0300

    feat: add table footer

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -2,11 +2,84 @@ import { Search } from "lucide-react";
 
 export function AttendeeList() {
   return (
-    <div className="flex gap-3 items-center">
-      <h1 className="text-2xl font-bold">Participantes</h1>
-      <div className="flex items-center gap-3 px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm">
-        <Search className="size-4 text-emerald-300" />
-        <input className="bg-transparent flex-1 outline-none" type="text" placeholder="Buscar participante..." name="" id="" />
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-3 items-center">
+        <h1 className="text-2xl font-bold">Participantes</h1>
+        <div className="flex items-center gap-3 px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm">
+          <Search className="size-4 text-emerald-300" />
+          <input
+            className="bg-transparent flex-1 outline-none"
+            type="text"
+            placeholder="Buscar participante..."
+            name=""
+            id=""
+          />
+        </div>
+      </div>
+
+      <div className="border border-white/10 rounded-lg">
+        <table className="w-full">
+          <thead>
+            <tr className="border-b border-white/10">
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                <input className="cursor-pointer" type="checkbox" />
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Código
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Participante
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Data de inscrição
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Data do check-in
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.from({ length: 10 }).map((_, id) => {
+              return (
+                <tr key={id} className="border-b border-white/10">
+                  <td className="py-3 px-4 text-sm text-zinc-300">
+                    <input className="cursor-pointer" type="checkbox" />
+                  </td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">64ba964b</td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">
+                    <div className="flex flex-col gap-1">
+                      <span className="font-semibold text-white">
+                        Gabriel Hideaki Souza Kumai
+                      </span>
+                      <span>hideaki@design.com.br</span>
+                    </div>
+                  </td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">
+                    7 dias atrás
+                  </td>
+                  <td className="py-3 px-4 text-sm text-zinc-300">
+                    3 dias atrás
+                  </td>
+                  <td className="py-3 px-4 text-sm text-zinc-300"></td>
+                </tr>
+              );
+            })}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
+                Mostrando 10 de 228 itens
+              </td>
+              <td
+                className="py-3 px-4 text-sm text-zinc-300 text-right"
+                colSpan={3}
+              >
+                Página 1 de 23
+              </td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     </div>
   );
