facd5a93462f8fa4e8572ef9a1aa092b1319a538
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-04-02 21:13:46 -0300

    feat: add Search input

---
+++ src/components/attendee-list.tsx
@@ -0,0 +1,13 @@
+import { Search } from "lucide-react";
+
+export function AttendeeList() {
+  return (
+    <div className="flex gap-3 items-center">
+      <h1 className="text-2xl font-bold">Participantes</h1>
+      <div className="flex items-center gap-3 px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm">
+        <Search className="size-4 text-emerald-300" />
+        <input className="bg-transparent flex-1 outline-none" type="text" placeholder="Buscar participante..." name="" id="" />
+      </div>
+    </div>
+  );
+}
