0583108754515f38a463ed14f9960ec803aad1f7
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-04-07 19:14:32 -0300

    feat: add url state search params

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -20,62 +20,93 @@ dayjs.locale("pt-br");
 dayjs.extend(relativeTime);
 
 interface Attendee {
-  id: string,
-  name: string,
-  email: string,
-  createdAt: string,
-  checkedInAt: string | null,
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+  checkedInAt: string | null;
 }
 
 export function AttendeeList() {
-  const [search, setSearch] = useState("");
-  const [page, setPage] = useState(1);
-  const [total, setTotal] = useState(0)
-  const [ attendees, setAttendees ] = useState<Attendee[]>([])
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString());
 
-  const totalPages = Math.ceil(total / 10)
+    if (url.searchParams.has("search")) {
+      return url.searchParams.get("search") ?? ""
+    }
+    return ""
+  });
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString());
+
+    if (url.searchParams.has("page")) {
+      return Number(url.searchParams.get("page"))
+    }
+    return 1
+  });
+
+  const [total, setTotal] = useState(0);
+  const [attendees, setAttendees] = useState<Attendee[]>([]);
+
+  const totalPages = Math.ceil(total / 10);
 
   useEffect(() => {
-    const url = new URL("http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees")
+    const url = new URL(
+      "http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees"
+    );
 
-    url.searchParams.set("pageIndex", String(page -1))
+    url.searchParams.set("pageIndex", String(page - 1));
     if (search.length > 0) {
-      url.searchParams.set("query", search)
+      url.searchParams.set("query", search);
     }
-   
 
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      setAttendees(data.attendees)
-      setTotal(data.total)
-    })
-  }, [page, search])
+      .then((response) => response.json())
+      .then((data) => {
+        setAttendees(data.attendees);
+        setTotal(data.total);
+      });
+  }, [page, search]);
 
-  function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value);
-    setPage(1)
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString());
+
+    url.searchParams.set("search", search);
+
+    window.history.pushState({}, "", url);
+
+    setSearch(search)
   }
 
-  function goToNextPage() {
-    if (page <= Math.ceil(totalPages - 1)) {
-      setPage(page + 1);
-    }
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString());
+
+    url.searchParams.set("page", String(page));
+
+    window.history.pushState({}, "", url);
+
+    setPage(page)
+  }
+
+  function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
+    setCurrentSearch(event.target.value);
+    setCurrentPage(1)
+  }
+
+  function goToNextPage() { 
+    setCurrentPage(page + 1);
   }
 
   function goToPreviousPage() {
-    if (page >= 2) {
-      setPage(page - 1);
-    }
+    setCurrentPage(page - 1);
   }
 
   function goToFirstPage() {
-    setPage(1);
+    setCurrentPage(1);
   }
 
   function goToLastPage() {
-    setPage(totalPages);
+    setCurrentPage(totalPages);
   }
 
   return (
@@ -88,6 +119,7 @@ export function AttendeeList() {
             className="bg-transparent flex-1 border-0 p-0 text-sm outline-none focus:ring-0"
             type="text"
             placeholder="Buscar participante..."
+            value={search}
             onChange={onSearchInputChanged}
           />
         </div>
@@ -138,7 +170,15 @@ export function AttendeeList() {
                   </div>
                 </TableCell>
                 <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
-                <TableCell>{attendee.checkedInAt === null ? <span className="inline-flex items-center rounded-md bg-zinc-400/10 px-2 py-1 text-xs font-medium text-zinc-400 ring-1 ring-inset ring-zinc-600/10">Não fez check-in</span> : dayjs().to(attendee.checkedInAt)}</TableCell>
+                <TableCell>
+                  {attendee.checkedInAt === null ? (
+                    <span className="inline-flex items-center rounded-md bg-zinc-400/10 px-2 py-1 text-xs font-medium text-zinc-400 ring-1 ring-inset ring-zinc-600/10">
+                      Não fez check-in
+                    </span>
+                  ) : (
+                    dayjs().to(attendee.checkedInAt)
+                  )}
+                </TableCell>
                 <TableCell>
                   <IconButton
                     transparent
@@ -168,10 +208,16 @@ export function AttendeeList() {
                   <IconButton onClick={goToPreviousPage} disabled={page === 1}>
                     <ChevronLeft className="size-4" />
                   </IconButton>
-                  <IconButton onClick={goToNextPage} disabled={page === totalPages}>
+                  <IconButton
+                    onClick={goToNextPage}
+                    disabled={page === totalPages}
+                  >
                     <ChevronRight className="size-4" />
                   </IconButton>
-                  <IconButton onClick={goToLastPage} disabled={page === totalPages}>
+                  <IconButton
+                    onClick={goToLastPage}
+                    disabled={page === totalPages}
+                  >
                     <ChevronsRight className="size-4" />
                   </IconButton>
                 </div>
